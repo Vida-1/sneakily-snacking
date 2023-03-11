77688e86f24b3fd91a182e2392a66813910acfe5
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-03-11 18:40:14 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import EffectDemo from './EffectDemo';
+import { socket } from './socket';
 
-function App() {
+const App = () => {
+  const [isConnect, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    function handleConnect() {
+      setIsConnected(true)
+      console.log("handleConnect has been triggered");
+    }
+
+    function handleDisconnect() {
+      setIsConnected(false);
+      console.log("handleDisconnect has been trieggered");
+    }
+
+    // theses are our listeners
+    socket.on("connect", handleConnect);
+    socket.on("disconnect", handleDisconnect);
+    socket.on("rsponse", (payload) => console.log(payload));
+
+    // clean up the socket listeners
+    return () => {
+      // turns off socket listeners
+      socket.off("connect", handleConnect);
+      socket.off("disconnect", handleDisconnect);
+      socket.off("response", () => console.log("response listener is off"));
+    };
+  });
+
+    const handleHello = () => {
+      socket.emit("hello");
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Is connected? {isConnected ? "true" : "false"}</p>
+      <button onClick={handleHello}>Say hello</button>
     </div>
   );
 }
 
-export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/EffectDemo.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+
+const EffectDemo = {} => {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+  //example one: runs once
+  useEffect(() => {
+    // just contains the first argument which is the callback function
+    // so this function will be called after every rednder
+    console.log("hi! this happens with every render")
+  })
+
+  //example 2: runs twice
+  useEffect(() => {
+    // contains the first argument and an empty dependancy array
+    // so this function will just on mount
+    console.log("hi! this happens on mount")
+  }, [])
+
+  // example 3
+  useEffect(() => {
+    // contains the first argument and a dependency array with a state variable in it to watch
+    // so this function will happen on mount, and every time count updates
+
+    console.log("hi! this happens when count updates and on mount ", count)
+  // don't do this:  setCount(count+1)  --basically don't set the state inside of the useEffect that will cause dependency array to change with each iteration, you'll get an infinitely called fxn
+  // you could do this because it is more restrictive:  if(count===2)
+  
+  }, [count])
+
+  
+  return (
+    <>
+      < button onClick={() => setCount(count + 1)}>Counter: {count}</button>
+      < button onClick={() => setCount(0)}>Reset</button>
+      <form>
+        <input onChange={(e) => setText(e.target.value)} value={text} />
+      </form>
+    </>
+  )
+}
+
+export default EffectDemo;
